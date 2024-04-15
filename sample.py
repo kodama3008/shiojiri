cadfa6c856196376611a0e2696804b841b23e398
児玉　英明（Kodama Hideaki） <cshd23008@g.nihon-u.ac.jp>
2024-04-15 13:56:18 +0900

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,23 @@
+import carla
+import random
+
+# Connect to the client and retrieve the world object
+client = carla.Client('localhost', 2000)
+world = client.get_world()
+
+# Set up the simulator in synchronous mode
+settings = world.get_settings()
+settings.synchronous_mode = True # Enables synchronous mode
+settings.fixed_delta_seconds = 0.05
+world.apply_settings(settings)
+
+# Set up the TM in synchronous mode
+traffic_manager = client.get_trafficmanager()
+traffic_manager.set_synchronous_mode(True)
+
+# Set a seed so behaviour can be repeated if necessary
+traffic_manager.set_random_device_seed(0)
+random.seed(0)
+
+# We will aslo set up the spectator so we can see what we do
+spectator = world.get_spectator()
